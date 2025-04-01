682ec176dad817dc41becc73e67f800e49d2dd19
Eber Lopz <eber.huinil012@gmail.com>
2025-03-31 22:18:54 -0600

    se agrego la configuracion de eureka como contenedor de microservicios

--- Car/src/main/java/CarService/Car/CarApplication.java
+++ Car/src/main/java/CarService/Car/CarApplication.java
@@ -2,8 +2,10 @@ package CarService.Car;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CarApplication {
 
 	public static void main(String[] args) {
